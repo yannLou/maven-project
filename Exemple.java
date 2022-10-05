a5cda6fb5d435d01bf33b4a46aece38a245ea567
tahararib <37878662+tahararib@users.noreply.github.com>
2022-10-05 15:41:00 +0200

    Create Exemple.java

---
+++ Exemple.java
@@ -0,0 +1,13 @@
+package com.example ;
+import javax.swing.JOptionPane ;
+
+public class Exemple {
+ 
+  public static void main (String[] args) {
+    // todo
+    system.out.println("Hello World") ;
+    try {
+    }catch(Exception e) {
+    }
+  }
+}
